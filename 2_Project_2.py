3b24fe428cf8b9e2ddd15174f7821083c2ce2631
florissimm <florissimmelink@gmail.com>
2025-09-09 18:59:11 +0200

    Add files via upload

---
+++ 2_Project_2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Project 2", layout="wide")
+st.title("📦 Project 2")
+st.info("Deze module komt binnenkort beschikbaar.")
---
+++ 5_Project_5.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Project 5", layout="wide")
+st.title("📦 Project 5")
+st.info("Deze module komt binnenkort beschikbaar.")
